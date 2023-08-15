c4fdff5958e26dd7028a8ab6b9bc011c65b132e9
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-08-15 20:59:52 +0545

    File name change

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log("hello friends");
+
+// values & variables
+
+var _myMother = "God";
+var _myName__ = "Ram";
+var $my__Name = "Hello ";
+console.log(_myName__);
+
+// Data types in java script
--- main.js
+++
@@ -1,2 +0,0 @@
-document.write("Hello from js")
-document.write('more more')
