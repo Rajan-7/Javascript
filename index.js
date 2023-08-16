e763df6ba55847a255fd21b5be706bdc5856e205
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-08-16 17:25:42 +0545

    Modification : index.js

--- index.js
+++ index.js
@@ -1,10 +1,89 @@
 // console.log("hello friends");
 
+
+
 // values & variables
 
 var _myMother = "God";
 var _myName__ = "Ram";
 var $my__Name = "Hello ";
-console.log(_myName__);
+// console.log(_myName__);
 
 // Data types in java script
+
+var myData = "Data";
+var myAge = 20;
+// console.log(myAge);
+
+// typeof operator
+// console.log(typeof(myAge));
+
+var isCorrect = true;
+// console.log(isCorrect);
+// console.log(typeof(isCorrect));
+
+// practices
+// concadinate
+// console.log(10+"5");
+// console.log(9-"5"); 
+// console.log("java"+" script");
+// console.log(" "+" ");
+// console.log(" "+"0");
+// console.log("Hari"-"Ram");
+// 1 represent true , 0 represent false
+// console.log(true+true);
+// console.log(false+false);
+// console.log(false-true);
+
+
+//  null vs undefined
+var useLess = null;
+// console.log(useLess);
+// console.log(typeof(useLess));
+
+var standBy;
+// console.log(standBy);
+// console.log(typeof(standBy));
+
+// NaN
+var isNumber = 9823242;
+// if(!isNaN(isNumber)){
+//     console.log("Great");
+// }
+// console.log(NaN === NaN);
+// console.log(Number.NaN === NaN);
+// console.log(isNaN(NaN));
+// console.log(Number.isNaN(NaN));
+
+// expressions & operators
+
+// assignment operator
+var x= 8;
+var y=8;
+// console.log(`Is both x & y are equal : ${x === y}`);
+
+// 2. arthmetic operator
+
+// console.log("Remainder " + 26%5);
+
+// increment & decrement operator
+
+// postfix: work with original value
+// var num = 11;
+// var newNum = num-- + 1;
+// console.log(num);
+// console.log(newNum);
+
+// prefix: work with updated value
+var num = 17;
+var newNum = --num + 3;
+console.log(num);
+console.log(newNum);
+
+
+
+
+
+
+
+
