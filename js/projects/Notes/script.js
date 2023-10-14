2819394b828bf21c50bb4555a882dee25e75e81a
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2023-10-14 23:01:05 +0545

    Notes complete

--- js/projects/Notes/script.js
+++ js/projects/Notes/script.js
@@ -1,9 +1,23 @@
 const newBtn = document.querySelector("#add");
 
+// localstorage function
+const updateLocalStorage = () => {
+  const textAreaData = document.querySelectorAll("textarea");
+  console.log(textAreaData);
+  const notes = [];
+  console.log(notes);
+  textAreaData.forEach((note) => {
+    return notes.push(note.value);
+  });
+
+  localStorage.setItem("noteid", JSON.stringify(notes));
+};
+
 const generateNewNote = (text = "") => {
   const note = document.createElement("div");
   note.classList.add("note");
-
+  
+  // html data
   const htmlData = `
     <div class="tools">
          <button class="edit"><i class="fas fa-edit"></i></button>
@@ -15,28 +29,37 @@ const generateNewNote = (text = "") => {
   note.insertAdjacentHTML("afterbegin", htmlData);
   document.body.appendChild(note);
 
-    //   Reference of edit & delete,div & textarea
-    const editButton = note.querySelector('.edit');
-    const delButton = note.querySelector('.delete');
-    const mainDiv = note.querySelector('.main');
-    const textArea = note.querySelector('textarea');
+  //   Reference of edit & delete,div & textarea
+  const editButton = note.querySelector(".edit");
+  const delButton = note.querySelector(".delete");
+  const mainDiv = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
 
-    // Deleting the node
-    delButton.addEventListener('click',()=>{
-        note.remove();
-    })
+  // Deleting the node
+  delButton.addEventListener("click", () => {
+    note.remove();
+    updateLocalStorage();
+  });
 
-    // Toggle the mainDiv & textarea
-    editButton.addEventListener('click',()=>{
-        mainDiv.classList.toggle('hidden');
-        textArea.classList.toggle('hidden');
-    })
+  // Toggle the mainDiv & textarea
+  textArea.value = text;
+  mainDiv.innerHTML = text;
 
-    textArea.addEventListener('change',(event)=>{
-        const userValue = event.target.value;
-        console.log(userValue);
-    })
+  editButton.addEventListener("click", () => {
+    mainDiv.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
 
+  textArea.addEventListener("change", (event) => {
+    const userValue = event.target.value;
+    mainDiv.innerHTML = userValue;
+    updateLocalStorage();
+  });
 };
+
+const notes = JSON.parse(localStorage.getItem("noteid"));
+if (notes) {
+  notes.forEach((note) => generateNewNote(note));
+}
 
 newBtn.addEventListener("click", () => generateNewNote());
